8a540bf53b543c43ec6eaa9633879f380888447f
mberg-ConU <mattybthemvp@gmail.com>
2022-10-27 20:12:03 -0400

    Added privacy agreement functionality.

---
+++ app/src/main/java/com/teamI/librarymonitoring/PrivacyActivity.java
@@ -0,0 +1,45 @@
+package com.teamI.librarymonitoring;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class PrivacyActivity extends AppCompatActivity {
+
+    protected Button btnAgree;
+    protected Button btnDisagree;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_privacy);
+
+        btnAgree = findViewById(R.id.btnAgree);
+        btnDisagree = findViewById(R.id.btnDisagree);
+
+        btnAgree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setPrivacyConsent(true);
+                finish();
+            }
+        });
+        btnDisagree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setPrivacyConsent(false);
+                // close the app
+                finishAffinity();
+            }
+        });
+    }
+
+    private void setPrivacyConsent(boolean bConsent){
+        SharedPreferencePrivacyUtility.setPrivacyConsent(this, bConsent);
+    }
+
+}
