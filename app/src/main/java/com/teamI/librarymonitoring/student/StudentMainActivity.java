a76e92c283f5338a86b069c8f14dacb38bd6eba9
red346 <kiratriar13@gmail.com>
2022-10-30 20:53:39 -0400

    updating StudentMainActivity

--- app/src/main/java/com/teamI/librarymonitoring/student/StudentMainActivity.java
+++ app/src/main/java/com/teamI/librarymonitoring/student/StudentMainActivity.java
@@ -2,9 +2,18 @@ package com.teamI.librarymonitoring.student;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.teamI.librarymonitoring.PrivacyActivity;
 import com.teamI.librarymonitoring.R;
+import com.teamI.librarymonitoring.SharedPreferencePrivacyUtility;
+import com.teamI.librarymonitoring.librarian.LibrarianNoiseLevelActivity;
+import com.teamI.librarymonitoring.librarian.LibrarianOccupancyActivity;
+import com.teamI.librarymonitoring.librarian.LibrarianSensorsConnectedActivity;
+import com.teamI.librarymonitoring.librarian.LibrarianSettingsActivity;
 
 public class StudentMainActivity extends AppCompatActivity {
 
@@ -14,3 +23,5 @@ public class StudentMainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_student_main);
     }
 }
+
+
