a060ce320698a6f3a15d32954caba1d21dd5682b
mabdullaht1 <mabdullaht@hotmail.com>
2022-10-31 18:57:46 -0400

    Completed task OD-1.5,

--- app/src/main/java/com/teamI/librarymonitoring/student/StudentSettingsActivity.java
+++ app/src/main/java/com/teamI/librarymonitoring/student/StudentSettingsActivity.java
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.PopupWindow;
 
 import com.teamI.librarymonitoring.PrivacyActivity;
 import com.teamI.librarymonitoring.R;
@@ -14,13 +18,14 @@ import com.teamI.librarymonitoring.librarian.LibrarianSettingsActivity;
 import com.teamI.utils.Preferences;
 
 public class StudentSettingsActivity extends AppCompatActivity {
-    Button btnLibrarian;
+    Button btnLibrarian, btncredittoconcordia_student;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_settings);
         btnLibrarian = findViewById(R.id.btnChangeToLibrarian);
+        btncredittoconcordia_student = findViewById(R.id.btncredittoconcordia_student);
         btnLibrarian.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -28,6 +33,27 @@ public class StudentSettingsActivity extends AppCompatActivity {
                 startActivity(intent);
                 Preferences.writeString(StudentSettingsActivity.this,"user","librarian");
                 finish();
+            }
+        });
+
+        btncredittoconcordia_student.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayoutInflater layoutInflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+
+                View viewcreditwindow = layoutInflater.inflate(R.layout.creditconcordia_layout,null);
+
+                PopupWindow popupWindow = new PopupWindow(viewcreditwindow,1000,1300,true);
+
+                popupWindow.showAtLocation(v, Gravity.CENTER,0,0);
+
+                viewcreditwindow.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        popupWindow.dismiss();
+                        return true;
+                    }
+                });
             }
         });
         setupButtons();
