6134e243d0d8fa7471e501a17c00f97af4134711
red346 <kiratriar13@gmail.com>
2022-10-30 23:13:38 -0400

    updating StudentMainActivity

--- app/src/main/java/com/teamI/librarymonitoring/student/StudentSettingsActivity.java
+++ app/src/main/java/com/teamI/librarymonitoring/student/StudentSettingsActivity.java
@@ -3,7 +3,8 @@ package com.teamI.librarymonitoring.student;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
 import com.teamI.librarymonitoring.R;
 
 public class StudentSettingsActivity extends AppCompatActivity {
@@ -12,5 +13,35 @@ public class StudentSettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_settings);
+
+        setupButtons();
+
+    }
+    private void setupButtons() {
+
+        Button btnFavorites = findViewById(R.id.btnFavorites);
+        btnFavorites.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Favorites();
+            }
+
+        });
+
+        Button btnDisplayPrivacyAgreement = findViewById(R.id.btnDisplayPrivacyAgreement);
+        btnDisplayPrivacyAgreement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                displayPrivacyAgreement();
+            }
+        }
+        );
+    }
+    private void Favorites(){
+
+    }
+    private void displayPrivacyAgreement(){
+        // TODO: display privacy agreement
+        // the agreement has not been written yet
     }
 }
