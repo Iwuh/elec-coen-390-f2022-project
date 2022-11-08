8e60f338a0766ea290d7baf6e9849cccec6a73ea
Matthew Faigan <matthew.faigan@gmail.com>
2022-11-08 13:47:44 -0500

    Finish EventArray class

--- Controller/sketch_controller_code/EventArray.h
+++ Controller/sketch_controller_code/EventArray.h
@@ -1,3 +1,5 @@
+#include <stdint.h>
+
 class EventArray
 {
 public:
@@ -6,8 +8,8 @@ public:
   {}
 
   // Add a new sensor trigger.
-  // timestamp_us: timestamp to add in microseconds
-  // returns true if there was space to add, otherwise false
+  // timestamp_us: The timestamp to add in microseconds.
+  // Returns true if there was space in the event array to add, otherwise false.
   bool add(uint64_t timestamp_us)
   {
     if (eventCount < ARRAY_MAX)
@@ -19,9 +21,9 @@ public:
   }
 
   // Match a microseconds timestamp based on calibration criteria.
-  // timestamp_us: the timestamp to match.
-  // returns the matched time stamp if it was found.
-  // Otherwise, returns the 64-bit unsigned maximum value
+  // timestamp_us: The timestamp to match in microseconds.
+  // Returns the matched time stamp if it was found. Removes the matched time stamp from the event array.
+  // Otherwise, returns the 64-bit unsigned maximum value.
   uint64_t findAndRemove(uint64_t timestamp_us)
   {
     // Perform a linear search over the array.
@@ -47,10 +49,53 @@ public:
     return 0xFFFFFFFFFFFFFFFF; 
   }
 
+  // Cleans all events from the array that are longer ago than MAX_TH.
+  // timestamp_us: The timestamp to compare to in microseconds.
+  // Returns the number of entries that were removed.
+  size_t clean(uint64_t timestamp_us)
+  {
+    size_t indicesToRemove[ARRAY_MAX];
+    size_t countToRemove = 0;
+    
+    // First pass: determine which indices need to be removed.
+    for (size_t i = 0; i < eventCount; i++)
+    {
+      uint64_t absdiff = abs(timestamp_us - events[i]); 
+      if (absdiff > MAX_TH)
+      {
+        indicesToRemove[countToRemove++] = i;
+      }
+    }
+
+    // Second pass: remove all events at those indices.
+    // Since removing the element at index i shifts all further elements back by one, we start at the end of the array and move backwards to make things easier.
+    for (size_t i = countToRemove - 1; i >= 0; i--)
+    {
+      for (size_t j = indicesToRemove[i]; j < eventCount - 1; j++)
+      {
+        events[j] = events[j+1];
+      }
+      eventCount--;      
+    }
+
+    return countToRemove;
+  }
+
+  // Returns the current number of entries in the array.
+  size_t count()
+  {
+    return eventCount;
+  }
+
 private:
+  // The maximum number of events that can be stored in the array.
   static constexpr size_t ARRAY_MAX = 20;
+  // The minimum time threshold between sensor triggers for a pair to be considered an enter or leave event.
   static constexpr uint64_t MIN_TH = 500000;
+  // The maximum time threshold between sensor triggers for a pair to be considered an enter or leave event.
+  // i.e., the difference between the pair must be greater than MIN_TH but less than MAX_TH.
   static constexpr uint64_t MAX_TH = 2000000;
+  
   uint64_t events[ARRAY_MAX];
   size_t eventCount;
 };
