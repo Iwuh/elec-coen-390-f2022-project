d6cbd360747a451ceed93c78298dc1d754d61b6f
Matthew Faigan <matthew.faigan@gmail.com>
2022-11-08 19:17:02 -0500

    Preparing for testing

--- Controller/sketch_controller_code/EventArray.h
+++ Controller/sketch_controller_code/EventArray.h
@@ -1,19 +1,15 @@
 #include <stdint.h>
 
-class EventArray
-{
+class EventArray {
 public:
   EventArray()
-    : eventCount(0)
-  {}
+    : eventCount(0) {}
 
   // Add a new sensor trigger.
   // timestamp_us: The timestamp to add in microseconds.
   // Returns true if there was space in the event array to add, otherwise false.
-  bool add(uint64_t timestamp_us)
-  {
-    if (eventCount < ARRAY_MAX)
-    {
+  bool add(int64_t timestamp_us) {
+    if (eventCount < ARRAY_MAX) {
       events[eventCount++] = timestamp_us;
       return true;
     }
@@ -23,67 +19,57 @@ public:
   // Match a microseconds timestamp based on calibration criteria.
   // timestamp_us: The timestamp to match in microseconds.
   // Returns the matched time stamp if it was found. Removes the matched time stamp from the event array.
-  // Otherwise, returns the 64-bit unsigned maximum value.
-  uint64_t findAndRemove(uint64_t timestamp_us)
-  {
+  // Otherwise, returns -1.
+  int64_t findAndRemove(int64_t timestamp_us) {    
     // Perform a linear search over the array.
     // I don't think a binary search will be worth it with such a small array size.
     // We can test this if time permits.
-    for (size_t i = 0; i < eventCount; i++)
-    {
+    for (size_t i = 0; i < eventCount; i++) {
       // Find the first event longer ago than MIN_TH but sooner than MAX_TH
-      uint64_t tdiff = timestamp_us - events[i];
-      if (tdiff >= MIN_TH && tdiff <= MAX_TH)
-      {
+      int64_t tdiff = timestamp_us - events[i];
+      if (tdiff >= MIN_TH && tdiff <= MAX_TH) {
         // If found, shift all subsequent elements back to remove this one and then return it.
-        uint64_t retval = events[i];
-        for (size_t j = i; j < eventCount - 1; j++)
-        {
-          events[j] = events[j+1];
+        int64_t retval = events[i];
+        for (size_t j = i; j < eventCount - 1; j++) {
+          events[j] = events[j + 1];
         }
         eventCount--;
         return retval;
       }
     }
-    // Maximum value of 64 bit unsigned integer, this will never be legitimately reached.
-    return 0xFFFFFFFFFFFFFFFF; 
+    // Indicates match not found.
+    return -1;
   }
 
   // Cleans all events from the array that are longer ago than MAX_TH.
   // timestamp_us: The timestamp to compare to in microseconds.
   // Returns the number of entries that were removed.
-  size_t clean(uint64_t timestamp_us)
-  {
+  size_t clean(int64_t timestamp_us) {
     size_t indicesToRemove[ARRAY_MAX];
     size_t countToRemove = 0;
-    
+
     // First pass: determine which indices need to be removed.
-    for (size_t i = 0; i < eventCount; i++)
-    {
-      uint64_t tdiff = timestamp_us - events[i]; 
-      if (tdiff > MAX_TH)
-      {
+    for (size_t i = 0; i < eventCount; i++) {
+      int64_t tdiff = timestamp_us - events[i];
+      if (tdiff > MAX_TH) {
         indicesToRemove[countToRemove++] = i;
       }
     }
 
     // Second pass: remove all events at those indices.
     // Since removing the element at index i shifts all further elements back by one, we start at the end of the array and move backwards to make things easier.
-    for (size_t i = countToRemove; i > 0; i--)
-    {
-      for (size_t j = indicesToRemove[i-1]; j < eventCount - 1; j++)
-      {
-        events[j] = events[j+1];
+    for (size_t i = countToRemove; i > 0; i--) {
+      for (size_t j = indicesToRemove[i - 1]; j < eventCount - 1; j++) {
+        events[j] = events[j + 1];
       }
-      eventCount--;      
+      eventCount--;
     }
 
     return countToRemove;
   }
 
   // Returns the current number of entries in the array.
-  size_t count()
-  {
+  size_t count() {
     return eventCount;
   }
 
@@ -91,11 +77,11 @@ private:
   // The maximum number of events that can be stored in the array.
   static constexpr size_t ARRAY_MAX = 20;
   // The minimum time threshold between sensor triggers for a pair to be considered an enter or leave event.
-  static constexpr uint64_t MIN_TH = 500000;
+  static constexpr int64_t MIN_TH = 500000;
   // The maximum time threshold between sensor triggers for a pair to be considered an enter or leave event.
   // i.e., the difference between the pair must be greater than MIN_TH but less than MAX_TH.
-  static constexpr uint64_t MAX_TH = 2000000;
-  
-  uint64_t events[ARRAY_MAX];
+  static constexpr int64_t MAX_TH = 2000000;
+
+  int64_t events[ARRAY_MAX];
   size_t eventCount;
 };
--- Controller/sketch_controller_code/sketch_controller_code.ino
+++ Controller/sketch_controller_code/sketch_controller_code.ino
@@ -1,68 +1,94 @@
 #include "EventArray.h"
 
-int ledPin1 = 5;                // choose the pins for the LEDs
-//int ledPin2 = 15;              
-int sensorPin1 =27;                
-int sensorPin2 = 26;            
-int pirState1 = LOW;            // assuming first no motion detected
+int ledPin1 = 5;  // choose the pins for the LEDs
+//int ledPin2 = 15;
+int sensorPin1 = 27;
+int sensorPin2 = 26;
+int pirState1 = LOW;  // assuming first no motion detected
 int pirState2 = LOW;
 
 int totalCount = 0;
 int currentCount = 0;
 
+EventArray sensorEvents_1;
+EventArray sensorEvents_2;
+
+// Return the system timestamp in microseconds.
+int64_t getTimeMicros() {
+  struct timeval tv_now;
+  gettimeofday(&tv_now, NULL);
+  return (int64_t)tv_now.tv_sec * 1000000L + (int64_t)tv_now.tv_usec;
+}
+
+// Polling
+/*
 void setup() {
-  pinMode(sensorPin1, INPUT_PULLUP);     // declare motion sensors as input
+  pinMode(sensorPin1, INPUT_PULLUP);  // declare motion sensors as input
   pinMode(sensorPin2, INPUT_PULLUP);
-  pinMode(ledPin1, OUTPUT);              // declare LED1 as output, the built in ESP32 chip led
+  pinMode(ledPin1, OUTPUT);  // declare LED1 as output, the built in ESP32 chip led
   //pinMode(ledPin2, OUTPUT);            // currently not functional due to lack of applicable LEDs
   Serial.begin(9600);
 }
 
 void loop() {
 
-  unsigned long currentMillisSensor1 = 0;           //  variables to store time when sensor state change happened
+  unsigned long currentMillisSensor1 = 0;  //  variables to store time when sensor state change happened
   unsigned long currentMillisSensor2 = 0;
   unsigned long previousMillisSensor1 = 0;
   unsigned long previousMillisSensor2 = 0;
 
   //Sensor 1
-  if (digitalRead(sensorPin1) == HIGH) {            // check if input1 is HIGH
-    digitalWrite(ledPin1, HIGH);                    // turn LED1 ON in that case
+  if (digitalRead(sensorPin1) == HIGH) {  // check if input1 is HIGH
+    digitalWrite(ledPin1, HIGH);          // turn LED1 ON in that case
     if (pirState1 == LOW) {
       // have just turned on
       Serial.println("Sensor 1: Motion detected!");
       // only want to print on the output change, not state
       pirState1 = HIGH;
-      delay(200);
-
+      int64_t now = getTimeMicros();
+      int64_t match = sensorEvents_2.findAndRemove(now);
+      if (match == -1) {
+        // no match found
+        sensorEvents_1.add(now);
+      } else {
+        // match found, this means a person has left
+        totalCount--;
+        Serial.println("A person has left.");
+      }
     }
   } else {
-    digitalWrite(ledPin1, LOW);                      // turn LED1 OFF
+    digitalWrite(ledPin1, LOW);  // turn LED1 OFF
     if (pirState1 == HIGH) {
       // have just turned off
       Serial.println("Sensor 1: Motion ended!");
       // only want to print on the output change, not state
       pirState1 = LOW;
-       
-      currentMillisSensor1 = millis();              // timestamp of Sensor1 state change stored
-      
+      currentMillisSensor1 = millis();
     }
   }
 
   //Sensor 2
-  if (digitalRead(sensorPin2) == HIGH) {            // check if input1 is HIGH
-     digitalWrite(ledPin1, HIGH);                   // turn LED1 ON in that case
+  if (digitalRead(sensorPin2) == HIGH) {  // check if input1 is HIGH
+    digitalWrite(ledPin1, HIGH);          // turn LED1 ON in that case
     //digitalWrite(ledPin2, HIGH);  // turn LED2 ON, currently not functional due to lack of applicable LEDs
     if (pirState2 == LOW) {
       // have just turned on
       Serial.println("Sensor 2: Motion detected!");
       // only want to print on the output change, not state
       pirState2 = HIGH;
-      delay(200);
-
+      int64_t now = getTimeMicros();
+      int64_t match = sensorEvents_1.findAndRemove(now);
+      if (match == -1) {
+        // no match found
+        sensorEvents_2.add(now);
+      } else {
+        // match found, this means a person has entered
+        totalCount++;
+        Serial.println("A person has entered.");
+      }
     }
   } else {
-    digitalWrite(ledPin1, LOW);                      //  turn LED1 OFF
+    digitalWrite(ledPin1, LOW);  //  turn LED1 OFF
     //digitalWrite(ledPin2, LOW); // turn LED2 OFF, currently not functional due to lack of applicable LEDs
     if (pirState2 == HIGH) {
       // have just turned off
@@ -70,83 +96,87 @@ void loop() {
       // only want to print on the output change, not state
       pirState2 = LOW;
 
-      currentMillisSensor2 = millis();               // timestamp of Sensor2 state change saved
-     
+      currentMillisSensor2 = millis();  // timestamp of Sensor2 state change saved
     }
   }
-  
+
   //Millis() approach
 
   //  comparing timestamps of sensor state change between the two sensors
   //  if Sensor1 state changed earlier than Sensor2 state => person entering the door
   //  otherwise => person leaving the door
 
-  if((previousMillisSensor1 != currentMillisSensor1) && (previousMillisSensor2 != currentMillisSensor2)) {    // update number of person entering or leaving only if timestamps of state changes for both sensors change
-    if(currentMillisSensor1 > currentMillisSensor2) {                                                        
-      totalCount++;
-      Serial.println("A person just entered.");
-    }
-    else {
-      totalCount--;
-      Serial.println("A person just left.");
-    }
-      Serial.print("Current Total: ");
-      Serial.println(totalCount);
+  //if ((previousMillisSensor1 != currentMillisSensor1) && (previousMillisSensor2 != currentMillisSensor2)) {  // update number of person entering or leaving only if timestamps of state changes for both sensors change
+    //if (currentMillisSensor1 > currentMillisSensor2) {
+      //totalCount++;
+      //Serial.println("A person just entered.");
+    //} else {
+     // totalCount--;
+    //  Serial.println("A person just left.");
+   // }
+    Serial.print("Current Total: ");
+    Serial.println(totalCount);
+    delay(50);
   }
 
   previousMillisSensor1 = currentMillisSensor1;
   previousMillisSensor2 = currentMillisSensor2;
-
 }
+*/
 
 
-/*
 //Tests and extra Examples
 
 //Example using interrupts
 
-// Set GPIOs for LED and PIR Motion Sensor
-const int led = 5;
-const int motionSensor = 27;
+void IRAM_ATTR sensor1_ISR() {
+  int64_t now = getTimeMicros();
+  int64_t match = sensorEvents_2.findAndRemove(now);
+  if (match == -1) {
+    // no match found
+    sensorEvents_1.add(now);
+  } else {
+    // match found, this means a person has left
+    totalCount--;
+  }  
+}
 
-// Timer: Auxiliary variables
-unsigned long now = millis();
-unsigned long lastTrigger = 0;
-boolean startTimer = false;
-
-// Checks if motion was detected, sets LED HIGH and starts a timer
-void IRAM_ATTR detectsMovement() {
-  Serial.println("MOTION DETECTED!!!");
-  digitalWrite(led, HIGH);
-  startTimer = true;
-  lastTrigger = millis();
+void IRAM_ATTR sensor2_ISR() {
+  int64_t now = getTimeMicros();
+  int64_t match = sensorEvents_1.findAndRemove(now);
+  if (match == -1) {
+    // no match found
+    sensorEvents_2.add(now);
+  } else {
+    // match found, this means a person has entered
+    totalCount++;
+  }  
 }
 
 void setup() {
   // Serial port for debugging purposes
-  Serial.begin(115200);
+  Serial.begin(9600);
   
   // PIR Motion Sensor mode INPUT_PULLUP
-  pinMode(motionSensor, INPUT);
+  pinMode(sensorPin1, INPUT_PULLUP);  
+  pinMode(sensorPin2, INPUT_PULLUP);
   // Set motionSensor pin as interrupt, assign interrupt function and set RISING mode
-  attachInterrupt(digitalPinToInterrupt(motionSensor), detectsMovement, RISING);
+  attachInterrupt(digitalPinToInterrupt(sensorPin1), sensor1_ISR, RISING);
+  attachInterrupt(digitalPinToInterrupt(sensorPin2), sensor2_ISR, RISING);
 
   // Set LED to LOW
-  pinMode(led, OUTPUT);
-  digitalWrite(led, LOW);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
 }
 
 void loop() {
-  // Current time
-  now = millis();
-  // Turn off the LED after the number of seconds defined in the timeSeconds variable
-  if(startTimer && (now - lastTrigger > (timeSeconds*1000))) {
-    Serial.println("Motion stopped...");
-    digitalWrite(led, LOW);
-    startTimer = false;
-  }
+  now = getTimeMicros();
+  Serial.print("Time: ");
+  Serial.print(now);
+  Serial.print(", Total count: ");
+  Serial.println(totalCount);
 }
-*/
+
 
 /*
 //Test - blinking LED
